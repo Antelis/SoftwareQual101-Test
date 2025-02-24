8e9ba1bdfc0e7cdf6c8609a0591d8a9779c13b4b
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2025-02-24 09:04:53 -0600

    Changed index.html and added test files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,22 @@ function calculate(){
 
     document.getElementById("result").innerText = "result: " + result;
 }
+function add(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    let result;
+    result = Number(num1) + Number(num2);
 
+    document.getElementById("result").innerText = "result: " + result;
+}
+function sub(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    let result;
+    result = Number(num1) - Number(num2);
+
+    document.getElementById("result").innerText = "result: " + result;
+}
 function resetFields(){
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
