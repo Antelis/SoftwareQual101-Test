da168c84b24cd0a6f395f6a1c4c1e9db2d5e7ce3
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2025-03-06 09:16:58 -0600

    Tests and everything set up like /add, /sub

---
+++ oci-macroservice/src/test/java/com/example/oci_macroservice/OciMacroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oci_macroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OciMacroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
